178db704568b1c32a906001be86de848891f376a
Mojisola-Akinrelere <mojisolaoakinrelere@gmail.com>
2024-02-04 19:03:25 +0100

    Get all elements with tablinks and tabcontent

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get all elements with the class 'tab-links' and 'tab-contents'
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-contents');
+
+
